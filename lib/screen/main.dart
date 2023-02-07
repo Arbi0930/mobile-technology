20b27984ceb264dd17c9fdcadf2d2be51faed999
Arbi0930 <mbsavage0930@gmail.com>
2023-02-07 16:25:46 +0800

    add

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -15,7 +15,10 @@ class _MainState extends State<Main> {
     ScaffoldMessenger.of(context).clearMaterialBanners();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text("Амжилттай хадгаллаа"),
+        content: Text(
+          "Амжилттай хадгаллаа.",
+          style: TextStyle(color: Colors.purple),
+        ),
         action: SnackBarAction(
             label: "Хаах",
             onPressed: () =>
@@ -33,98 +36,106 @@ class _MainState extends State<Main> {
           style: TextStyle(fontSize: 15),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Text("Дуртай амьтанаа сонгоно уу."),
-          ),
-          Column(
-            children: [
-              Radio(
-                  value: 1,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Text(
+                "Дуртай амьтанаа сонгоно уу.",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+            ),
+            Column(
+              children: [
+                Radio(
+                    value: 1,
+                    groupValue: _value,
+                    onChanged: (value) {
+                      setState(() {
+                        _value = value!;
+                      });
+                    }),
+                Text(
+                  "Туулай",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Image.asset(
+                  'images/rabbit.jpg',
+                  scale: 1.5,
+                  height: 75,
+                ),
+                SizedBox(height: 10),
+                Radio(
+                  value: 2,
                   groupValue: _value,
                   onChanged: (value) {
                     setState(() {
                       _value = value!;
                     });
-                  }),
-              Text(
-                "Туулай",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Image.asset(
-                'images/rabbit.jpg',
-                scale: 1.5,
-                height: 75,
-              ),
-              SizedBox(height: 10),
-              Radio(
-                value: 2,
-                groupValue: _value,
-                onChanged: (value) {
-                  setState(() {
-                    _value = value!;
-                  });
-                },
-              ),
-              Text(
-                "Үнэг",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Image.asset(
-                'images/fox.jpg',
-                scale: 1.5,
-                height: 80,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Radio(
-                value: 3,
-                groupValue: _value,
-                onChanged: (value) {
-                  setState(() {
-                    _value = value!;
-                  });
-                },
-              ),
-              Text(
-                "Шар шувуу",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Image.asset(
-                'images/owl.jpg',
-                scale: 1.4,
-                height: 90,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                height: 50,
-                width: 150,
-                child: ElevatedButton(
-                  onPressed: _onSubmit,
-                  child: Text(
-                    "Илгээх",
-                    style: TextStyle(fontSize: 16),
+                  },
+                ),
+                Text(
+                  "Үнэг",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Image.asset(
+                  'images/fox.jpg',
+                  scale: 1.5,
+                  height: 80,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Radio(
+                  value: 3,
+                  groupValue: _value,
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value!;
+                    });
+                  },
+                ),
+                Text(
+                  "Шар шувуу",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Image.asset(
+                  'images/owl.jpg',
+                  scale: 1.4,
+                  height: 90,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 150,
+                  child: ElevatedButton(
+                    onPressed: _onSubmit,
+                    child: Text(
+                      "Илгээх",
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
